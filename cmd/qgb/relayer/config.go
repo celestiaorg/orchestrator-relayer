421aa14c768abdf19c3c61844529780cfc3ff443
CHAMI Rachid <chamirachid1@gmail.com>
2023-01-29 11:47:08 +0100

    chore: changes for golang ci lint to pass (#80)
    * chore: chore add nolint to parameters that will be used later
    
    * chore: chore add nolint to parameters that will be used later
    
    * chore: comment unused code for CI to pass
    
    * chore: rename commands for revive to pass

--- cmd/qgb/relayer/config.go
+++ cmd/qgb/relayer/config.go
@@ -13,15 +13,15 @@ import (
 )
 
 const (
-	celestiaChainIDFlag = "celes-chain-id"
+	// celestiaChainIDFlag = "celes-chain-id"
 	privateKeyFlag      = "eth-priv-key"
 	evmChainIDFlag      = "evm-chain-id"
 	celesGRPCFlag       = "celes-grpc"
 	tendermintRPCFlag   = "celes-http-rpc"
 	evmRPCFlag          = "evm-rpc"
 	contractAddressFlag = "contract-address"
-	startingNonceFlag   = "starting-nonce"
-	evmGasLimitFlag     = "evm-gas-limit"
+	// startingNonceFlag = "starting-nonce"
+	evmGasLimitFlag = "evm-gas-limit"
 )
 
 func addRelayerFlags(cmd *cobra.Command) *cobra.Command {
