26041730d46e326dd5dabea7796200b885186589
rachid <chamirachid1@gmail.com>
2023-02-10 10:32:37 +0100

    fix: skip querier tests in short mode

--- rpc/suite_test.go
+++ rpc/suite_test.go
@@ -23,6 +23,14 @@ type QuerierTestSuite struct {
 
 func (s *QuerierTestSuite) SetupSuite() {
 	t := s.T()
+	if testing.Short() {
+		// The main reason for skipping these tests in short mode is to avoid detecting unrelated
+		// race conditions.
+		// In fact, this test suite uses an existing Celestia-app node to simulate a real environment
+		// to execute tests against. However, this leads to data races in multiple areas.
+		// Thus, we can skip them as the races detected are not related to this repo.
+		t.Skip("skipping queriers tests in short mode.")
+	}
 	s.Network = qgbtesting.NewCelestiaNetwork(t, time.Millisecond)
 	_, err := s.Network.WaitForHeightWithTimeout(400, 30*time.Second)
 	s.EncConf = encoding.MakeConfig(app.ModuleEncodingRegisters...)
