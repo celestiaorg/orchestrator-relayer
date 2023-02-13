ea4404e030b270aa2946b88d45fa44cd41b0816c
Rahul Ghangas <rahulghangas0@gmail.com>
2023-02-13 09:47:50 +0530

    chore: register sdk config

--- cmd/orchrelayer/cmd/root.go
+++ cmd/orchrelayer/cmd/root.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"github.com/celestiaorg/celestia-app/app"
 	"github.com/celestiaorg/orchestrator-relayer/x/qgb/orchestrator"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
 )
 
@@ -10,6 +12,12 @@ const EnvPrefix = "CELESTIA"
 // NewRootCmd creates a new root command for celestia-appd. It is called once in the
 // main function.
 func NewRootCmd() *cobra.Command {
+	cfg := sdk.GetConfig()
+	cfg.SetBech32PrefixForAccount(app.Bech32PrefixAccAddr, app.Bech32PrefixAccPub)
+	cfg.SetBech32PrefixForValidator(app.Bech32PrefixValAddr, app.Bech32PrefixValPub)
+	cfg.SetBech32PrefixForConsensusNode(app.Bech32PrefixConsAddr, app.Bech32PrefixConsPub)
+	cfg.Seal()
+
 	rootCmd := &cobra.Command{
 		Use:   "celestia-appd",
 		Short: "Start celestia app",
