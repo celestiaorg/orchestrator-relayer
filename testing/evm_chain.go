b94ec814f81a08fb492f7414459e9d5128b6ede0
CHAMI Rachid <chamirachid1@gmail.com>
2023-02-14 09:14:42 +0100

    docs: document the test chain ID and put it in a const (#122)

--- testing/evm_chain.go
+++ testing/evm_chain.go
@@ -12,8 +12,11 @@ import (
 	"github.com/ethereum/go-ethereum/core"
 )
 
-// EVMChain is a wrapped geth simulated backend which will be used to simulate an EVM chain.
-// The resulting chain always has chainID 1337.
+// EVMTestNetworkChainID the test EVM network chain ID.
+const EVMTestNetworkChainID = 1337
+
+// EVMChain is a wrapped Geth simulated backend which will be used to simulate an EVM chain.
+// The resulting test chain has always 1337 as a chain ID.
 type EVMChain struct {
 	Auth         *bind.TransactOpts
 	GenesisAlloc core.GenesisAlloc
@@ -23,7 +26,7 @@ type EVMChain struct {
 }
 
 func NewEVMChain(key *ecdsa.PrivateKey) *EVMChain {
-	auth, err := bind.NewKeyedTransactorWithChainID(key, big.NewInt(1337))
+	auth, err := bind.NewKeyedTransactorWithChainID(key, big.NewInt(EVMTestNetworkChainID))
 	if err != nil {
 		panic(err)
 	}
@@ -43,7 +46,7 @@ func NewEVMChain(key *ecdsa.PrivateKey) *EVMChain {
 		GenesisAlloc: gAlloc,
 		Backend:      backend,
 		Key:          key,
-		ChainID:      1337,
+		ChainID:      EVMTestNetworkChainID,
 	}
 }
 
