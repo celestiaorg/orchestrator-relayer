205f417e7e8538847e3a3d78945b4cb5085d4ee5
CHAMI Rachid <chamirachid1@gmail.com>
2023-02-17 22:11:40 +0100

    test: add relayer tests (#135)
    * test: add relayer tests
    
    * fix: update implementation and periodic commit
    
    * fix: increase wait for height
    
    * fix: increase wait for height
    
    * fix: skip relayer test in race mode

---
+++ relayer/suite_test.go
@@ -0,0 +1,47 @@
+package relayer_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/celestiaorg/orchestrator-relayer/orchestrator"
+
+	"github.com/celestiaorg/orchestrator-relayer/relayer"
+	qgbtesting "github.com/celestiaorg/orchestrator-relayer/testing"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type RelayerTestSuite struct {
+	suite.Suite
+	Node         *qgbtesting.TestNode
+	Orchestrator *orchestrator.Orchestrator
+	Relayer      *relayer.Relayer
+}
+
+func (s *RelayerTestSuite) SetupSuite() {
+	t := s.T()
+	if testing.Short() {
+		t.Skip("skipping relayer tests in short mode.")
+	}
+	ctx := context.Background()
+	s.Node = qgbtesting.NewTestNode(ctx, t)
+	_, err := s.Node.CelestiaNetwork.WaitForHeight(2)
+	require.NoError(t, err)
+	s.Orchestrator = qgbtesting.NewOrchestrator(s.Node)
+	s.Relayer = qgbtesting.NewRelayer(s.Node)
+	go s.Node.EVMChain.PeriodicCommit(ctx, time.Millisecond)
+	initVs, err := s.Relayer.AppQuerier.QueryValsetByNonce(s.Node.Context, 1)
+	require.NoError(t, err)
+	_, _, _, err = s.Relayer.EVMClient.DeployQGBContract(s.Node.EVMChain.Auth, s.Node.EVMChain.Backend, *initVs, 1, true)
+	require.NoError(t, err)
+}
+
+func (s *RelayerTestSuite) TearDownSuite() {
+	s.Node.Close()
+}
+
+func TestRelayer(t *testing.T) {
+	suite.Run(t, new(RelayerTestSuite))
+}
--- testing/qgb.go
+++ testing/qgb.go
@@ -9,11 +9,27 @@ import (
 	celestiatestnode "github.com/celestiaorg/celestia-app/testutil/testnode"
 	"github.com/celestiaorg/orchestrator-relayer/orchestrator"
 	"github.com/celestiaorg/orchestrator-relayer/p2p"
+	"github.com/celestiaorg/orchestrator-relayer/relayer"
 	"github.com/celestiaorg/orchestrator-relayer/rpc"
 
 	"github.com/celestiaorg/orchestrator-relayer/evm"
 	tmlog "github.com/tendermint/tendermint/libs/log"
 )
+
+func NewRelayer(
+	node *TestNode,
+) *relayer.Relayer {
+	logger := tmlog.NewNopLogger()
+	appQuerier := rpc.NewAppQuerier(logger, node.CelestiaNetwork.GRPCClient, encoding.MakeConfig(app.ModuleEncodingRegisters...))
+	tmQuerier := rpc.NewTmQuerier(node.CelestiaNetwork.Client, logger)
+	p2pQuerier := p2p.NewQuerier(node.DHTNetwork.DHTs[0], logger)
+	evmClient := NewEVMClient(node.EVMChain.Key)
+	r, err := relayer.NewRelayer(tmQuerier, appQuerier, p2pQuerier, evmClient, logger)
+	if err != nil {
+		panic(err)
+	}
+	return r
+}
 
 func NewEVMClient(key *ecdsa.PrivateKey) *evm.Client {
 	logger := tmlog.NewNopLogger()
