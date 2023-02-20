99ed6fe0c0cd197c1f5232ce313c4fcd012ed6f8
CHAMI Rachid <chamirachid1@gmail.com>
2023-02-20 14:54:26 +0100

    feat: skip all the tests in race mode if they use Celestia testnode (#155)

--- testing/celestia_network.go
+++ testing/celestia_network.go
@@ -20,6 +20,14 @@ type CelestiaNetwork struct {
 // Uses `testing.T` to fail if an error happens.
 // Only supports the creation of a single validator currently.
 func NewCelestiaNetwork(t *testing.T, blockTime time.Duration) *CelestiaNetwork {
+	if testing.Short() {
+		// The main reason for skipping these tests in short mode is to avoid detecting unrelated
+		// race conditions.
+		// In fact, this test suite uses an existing Celestia-app node to simulate a real environment
+		// to execute tests against. However, this leads to data races in multiple areas.
+		// Thus, we can skip them as the races detected are not related to this repo.
+		t.Skip("skipping tests in short mode.")
+	}
 	accounts, clientContext := celestiatestnode.DefaultNetwork(t, blockTime)
 	return &CelestiaNetwork{
 		Context:   clientContext,
