7f1f581a6410af7bf7c31787c1ce30cbd1bfc557
CHAMI Rachid <chamirachid1@gmail.com>
2023-03-31 15:29:42 +0200

    Update store/errors.go

--- store/errors.go
+++ store/errors.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	// ErrOpened is thrown on attempt to open already open/in-use Store.
-	ErrOpened = errors.New("node: store is in use")
+	ErrOpened = errors.New("store is in use")
 	// ErrNotInited is thrown on attempt to open Store without initialization.
 	ErrNotInited = errors.New("node: store is not initialized")
 )
