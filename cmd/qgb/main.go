adaddd66c0a73a96b717de7a718f16c9fdaf87e4
rachid <chamirachid1@gmail.com>
2023-04-18 17:58:51 +0200

    chore: move the root command to a separate package to be importable

--- cmd/qgb/main.go
+++ cmd/qgb/main.go
@@ -2,43 +2,13 @@ package main
 
 import (
 	"context"
+	"github.com/celestiaorg/orchestrator-relayer/cmd/qgb/root"
 	"os"
-
-	"github.com/celestiaorg/orchestrator-relayer/cmd/qgb/generate"
-
-	"github.com/celestiaorg/celestia-app/x/qgb/client"
-	"github.com/celestiaorg/orchestrator-relayer/cmd/qgb/deploy"
-	"github.com/celestiaorg/orchestrator-relayer/cmd/qgb/orchestrator"
-	"github.com/celestiaorg/orchestrator-relayer/cmd/qgb/relayer"
-
-	"github.com/spf13/cobra"
 )
 
 func main() {
-	rootCmd := NewRootCmd()
+	rootCmd := root.Cmd()
 	if err := rootCmd.ExecuteContext(context.Background()); err != nil {
 		os.Exit(1)
 	}
-}
-
-// NewRootCmd creates a new root command for the QGB CLI. It is called once in the
-// main function.
-func NewRootCmd() *cobra.Command {
-	rootCmd := &cobra.Command{
-		Use:          "qgb",
-		Short:        "The Quantum-Gravity-Bridge CLI",
-		SilenceUsage: true,
-	}
-
-	rootCmd.AddCommand(
-		orchestrator.Command(),
-		relayer.Command(),
-		deploy.Command(),
-		client.VerifyCmd(),
-		generate.Command(),
-	)
-
-	rootCmd.SetHelpCommand(&cobra.Command{})
-
-	return rootCmd
 }
