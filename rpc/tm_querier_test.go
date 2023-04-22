130207a72631024330113879a43c4a98e83d70d1
rachid <chamirachid1@gmail.com>
2023-04-22 04:03:54 +0200

    feat: wait for block 1 before enqueuing missing nonces

--- rpc/tm_querier_test.go
+++ rpc/tm_querier_test.go
@@ -32,6 +32,45 @@ func (s *QuerierTestSuite) TestQueryCommitment() {
 	assert.Equal(t, expectedCommitment.DataCommitment, actualCommitment)
 }
 
+func (s *QuerierTestSuite) TestQueryHeight() {
+	t := s.T()
+	_, err := s.Network.WaitForHeight(101)
+	require.NoError(t, err)
+
+	tmQuerier := rpc.NewTmQuerier(
+		s.Network.RPCAddr,
+		tmlog.NewNopLogger(),
+	)
+	tmQuerier.WithClientConn(s.Network.Client)
+
+	height, err := tmQuerier.QueryHeight(context.Background())
+	require.NoError(t, err)
+
+	assert.Greater(t, height, int64(101))
+}
+
+func (s *QuerierTestSuite) TestWaitForHeight() {
+	t := s.T()
+	_, err := s.Network.WaitForHeight(10)
+	require.NoError(t, err)
+
+	tmQuerier := rpc.NewTmQuerier(
+		s.Network.RPCAddr,
+		tmlog.NewNopLogger(),
+	)
+	tmQuerier.WithClientConn(s.Network.Client)
+
+	height, err := tmQuerier.QueryHeight(context.Background())
+	require.NoError(t, err)
+
+	err = tmQuerier.WaitForHeight(context.Background(), height+20)
+	require.NoError(t, err)
+
+	currentHeight, err := tmQuerier.QueryHeight(context.Background())
+	require.NoError(t, err)
+	assert.GreaterOrEqual(t, currentHeight, height+20)
+}
+
 func (s *QuerierTestSuite) TestSubscribeEvents() {
 	t := s.T()
 	_, err := s.Network.WaitForHeight(101)
