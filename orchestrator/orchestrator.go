0508c24058c1267e8dc5640db07de01b590f17a1
CHAMI Rachid <chamirachid1@gmail.com>
2023-11-21 18:43:50 +0100

    chore: make the validator not part of valset log info level (#611)

--- orchestrator/orchestrator.go
+++ orchestrator/orchestrator.go
@@ -280,7 +280,7 @@ func (orch Orchestrator) Process(ctx context.Context, nonce uint64) error {
 			orch.Logger.Debug("failed to query last valset before nonce (most likely pruned). signing anyway", "err", err.Error())
 		} else if !ValidatorPartOfValset(previousValset.Members, orch.EvmAccount.Address.Hex()) {
 			// no need to sign if the orchestrator is not part of the validator set that needs to sign the attestation
-			orch.Logger.Debug("validator not part of valset. won't sign", "nonce", nonce)
+			orch.Logger.Info("validator not part of valset. won't sign", "nonce", nonce)
 			return nil
 		}
 
